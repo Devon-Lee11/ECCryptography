d9f4be118923c93eb758070842b6f67112548cb0
Devon Lee <devonislee@gmail.com>
2022-02-02 11:25:59 -0500

    moved all testing code into tests.py

--- ECCInterface/main.py
+++ ECCInterface/main.py
@@ -1,14 +1,13 @@
-from ECCInterface import generate_keys, sign, verify_data
-from tests import test_sign_with_array_data, test_verify_data_with_array_data
+from ECCInterface import generate_keys
+from tests import test_sign_with_array_data, test_verify_data_with_array_data, \
+    test_verify_data_with_file, test_sign_with_file
 
 
 def main():
     private_key, public_key = generate_keys()
     print(private_key, public_key)
-    signed_document = sign('data.txt', private_key)
-    print(signed_document)
-    verified_data = verify_data('data.txt', signed_document, public_key)
-    print(verified_data)
+    test_verify_data_with_file()
+    test_sign_with_file()
     test_sign_with_array_data()
     test_verify_data_with_array_data()
 
--- ECCInterface/tests.py
+++ ECCInterface/tests.py
@@ -1,6 +1,7 @@
 from ECCInterface import generate_keys, sign, verify_data
 private_key, public_key = generate_keys()
 test_array = [1, 2, 3]
+signed_document = sign('data.txt', private_key)
 
 
 def test_sign_with_array_data():
@@ -11,3 +12,12 @@ def test_sign_with_array_data():
 def test_verify_data_with_array_data():
     print('expected false')
     print(sign(test_array, public_key))
+
+
+def test_sign_with_file():
+    print(signed_document)
+
+
+def test_verify_data_with_file():
+    verified_data = verify_data('data.txt', signed_document, public_key)
+    print(verified_data)
