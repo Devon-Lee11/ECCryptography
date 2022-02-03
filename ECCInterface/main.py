2a6ad7bb6fb6cad2745488e056b8ff7d921f4df0
Devon Lee <devonislee@gmail.com>
2022-02-03 11:54:54 -0500

    added new test

--- ECCInterface/main.py
+++ ECCInterface/main.py
@@ -1,5 +1,6 @@
 from tests import test_sign_with_array_data, test_verify_data_with_array_data, \
-    test_verify_data_with_file, test_sign_with_file, test_keys_were_generated
+    test_verify_data_with_file, test_sign_with_file, test_keys_were_generated, \
+    test_sign_with_string
 
 
 def main():
@@ -8,6 +9,7 @@ def main():
     test_sign_with_file()
     test_sign_with_array_data()
     test_verify_data_with_array_data()
+    test_sign_with_string()
 
 
 if __name__ == "__main__":
--- ECCInterface/tests.py
+++ ECCInterface/tests.py
@@ -1,7 +1,10 @@
 from ECCInterface import generate_keys, sign, verify_data
+
 private_key, public_key = generate_keys()
 test_array = [1, 2, 3]
 signed_document = sign('data.txt', private_key)
+test_string = 'this is data'
+signed_test_string = sign(test_string, private_key)
 
 
 def test_sign_with_array_data():
@@ -24,3 +27,7 @@ def test_verify_data_with_file():
 
 def test_keys_were_generated():
     print(public_key, private_key)
+
+
+def test_sign_with_string():
+    print(signed_test_string)
